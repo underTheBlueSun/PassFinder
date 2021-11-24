0becdcd525c99d737dc57d4d370f1dc57fa05004
underTheBlueSun <wind7129@naver.com>
2021-11-25 08:26:02 +0900

    Initial Commit

---
+++ PassFinder/PassFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassFinderApp.swift
+//  PassFinder
+//
+//  Created by macbook on 2021/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PassFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
