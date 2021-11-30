148a71392d72cfc13772f7a36893b8182f962434
underTheBlueSun <wind7129@naver.com>
2021-12-01 07:39:54 +0900

    ..

---
+++ PassFinder/Example/Test3.swift
@@ -0,0 +1,27 @@
+//
+//  Test3.swift
+//  PassFinder
+//
+//  Created by underTheBlueSun on 2021/12/01.
+//
+
+import SwiftUI
+
+struct Test3: View {
+    var body: some View {
+        
+        VStack {
+            
+            Color.pink.ignoresSafeArea()
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        
+    }
+}
+
+struct Test3_Previews: PreviewProvider {
+    static var previews: some View {
+        Test3()
+    }
+}
