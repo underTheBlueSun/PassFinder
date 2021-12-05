13de50744bb7d531bddb918f65f0bd47e0f6384b
underTheBlueSun <wind7129@naver.com>
2021-12-05 17:48:30 +0900

    ..

--- PassFinder/MyTypeView.swift
+++ PassFinder/MyTypeView.swift
@@ -15,34 +15,52 @@ struct MyTypeView: View {
         
         VStack {
             
-            VStack {
+            Text("나의 유형")
+//                .frame(width: 320, alignment: .leading)
+                .foregroundColor(.white)
+                .font(.system(size: 30, weight: .heavy))
+//                .padding()
+            
+            VStack(spacing: 0) {
                 
                 HStack {
                     
+                    VStack(spacing: 0) {
+                        
+                        Image("ENTJ")
+                            .resizable()
+                            .cornerRadius(10)
+                            .frame(width: 100, height: 90)
+                        
+        //                Text(passFinderModel.myType)
+                        Text("ISTP")
+                            .foregroundColor(.systemTeal)
+                            .font(.system(size: 25, weight: .heavy))
+                            .padding(.vertical, 1)
+                        
+                        Divider().frame(width:100).background(Color.gray)
+                        
+        //                Text(passFinderModel.filteredArray[passFinderModel.filteredArray.startIndex].title)
+                        Text("잔다르크형")
+                            .foregroundColor(.passFinderItem1)
+                            .font(.system(size: 13, weight: .heavy))
+                            .padding(.vertical, 2)
+                    } // vstack
+                    .padding()
                     
-                }
+                    ScrollView {
+                        
+                        Text("dsfasfasdfsdfsdafsdafsdafdsafsdfasdfsdafsdafsdfsdafdsfdsfdsfdsfdsfdfdsfds")
+                    }
+                    .frame(width: 180, height: 150)
+                    
+                } // hstack
                 
                 
                 
-                Text("나의 타입은")
-                    .foregroundColor(.gray)
-                    .padding(.vertical, 1)
-                
-//                Text(passFinderModel.myType)
-                Text("ISTP")
-                    .foregroundColor(.systemTeal)
-                    .font(.system(size: 30, weight: .heavy))
-                    .padding(.vertical, 1)
-                
-                Divider().frame(width:100).background(Color.gray)
-                
-//                Text(passFinderModel.filteredArray[passFinderModel.filteredArray.startIndex].title)
-                Text("잔다르크형")
-                    .foregroundColor(.passFinderItem1)
-                    .font(.system(size: 18, weight: .heavy))
                 
             }
-            .frame(width: 300, height: 200)
+            .frame(width: 350, height: 200)
             .background(.white)
             .cornerRadius(10)
             
