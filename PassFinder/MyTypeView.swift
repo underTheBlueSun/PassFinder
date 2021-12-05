3d60ddf10e9cf7e7335b201a270e841a580b9706
underTheBlueSun <wind7129@naver.com>
2021-12-06 07:35:47 +0900

    ..

--- PassFinder/MyTypeView.swift
+++ PassFinder/MyTypeView.swift
@@ -13,66 +13,85 @@ struct MyTypeView: View {
     
     var body: some View {
         
-        VStack {
+        ScrollView {
             
-            Text("나의 유형")
-//                .frame(width: 320, alignment: .leading)
-                .foregroundColor(.white)
-                .font(.system(size: 28, weight: .heavy))
-            
-            VStack(spacing: 0) {
+            VStack(spacing:0) {
                 
-                HStack(spacing: 20) {
+                HStack {
                     
-                    Image("ENTJ")
-                        .resizable()
-                        .cornerRadius(10)
-                        .frame(width: 120, height: 130)
+                    Image("category1").resizable().frame(width: 40, height: 40)
                     
-                    VStack(spacing: 0) {
-                        
-                        Text("ISTP")
-                            .foregroundColor(.systemTeal)
-                            .font(.system(size: 45, weight: .heavy))
-                            .padding(.vertical, 1)
-                        
-                        Divider().frame(width:100).background(Color.gray)
-                        
-        //                Text(passFinderModel.filteredArray[passFinderModel.filteredArray.startIndex].title)
-                        Text("잔다르크형")
-                            .foregroundColor(.passFinderItem1)
-                            .font(.system(size: 20, weight: .heavy))
-                            .padding(.vertical, 2)
-                        
-                    } // vstack
+                    Text("나의 유형")
+                        .foregroundColor(.white)
+                        .font(.system(size: 25, weight: .heavy))
                     
                     
-                } // hstack
+                }
+                
+                VStack(spacing: 0) {
+                    
+                    HStack(spacing: 20) {
+                        
+                        Image("ENTJ")
+                            .resizable()
+                            .cornerRadius(10)
+                            .frame(width: 120, height: 130)
+                        
+                        VStack(spacing: 0) {
+                            
+                            Text("ISTP")
+                                .foregroundColor(.systemTeal)
+                                .font(.system(size: 45, weight: .heavy))
+                                .padding(.vertical, 1)
+                            
+                            Divider().frame(width:100).background(Color.gray)
+                            
+            //                Text(passFinderModel.filteredArray[passFinderModel.filteredArray.startIndex].title)
+                            Text("잔다르크형")
+                                .foregroundColor(.passFinderItem1)
+                                .font(.system(size: 20, weight: .heavy))
+                                .padding(.vertical, 2)
+                            
+                        } // vstack
+                        
+                        
+                    } // hstack
+                    .padding()
+                    
+                    ScrollView {
+                        
+                        Text("dsfasfasdfsdfsdafsdafsdafdsafsdfasdfsdafsdafsdfsdafdsfdsfdsfdsfdsfdfdsfdsdfsdfsdfdsfdsfsdfsdfssdfsdfdsfdsfsdfdsfdsfdsfdsfsdfsdfdsfsdfsdfsdfsdfdsfsdfsdfdfghfhgf;hl;l;dfm34054305i4395-2340djvxcljewrkpwerkpeqewpripewrperperepdffjsdkf;sadkfaskf;asdfk")
+                    }
+                    .frame(width: 270, height: 120)
+                    
+                } // vstack
+                .frame(width: 350, height: 320)
+                .background(.white)
+                .cornerRadius(10)
                 .padding()
                 
-                ScrollView {
+                VStack(spacing: 0) {
                     
-                    Text("dsfasfasdfsdfsdafsdafsdafdsafsdfasdfsdafsdafsdfsdafdsfdsfdsfdsfdsfdfdsfdsdfsdfsdfdsfdsfsdfsdfssdfsdfdsfdsfsdfdsfdsfdsfdsfsdfsdfdsfsdfsdfsdfsdfdsfsdfsdfdfghfhgf;hl;l;dfm34054305i4395-2340djvxcljewrkpwerkpeqewpripewrperperepdffjsdkf;sadkfaskf;asdfk")
-                }
-                .frame(width: 270, height: 120)
+                    Text("aaaa")
+                    
+                } // vstack
+                .frame(width: 350, height: 300)
+                .background(.white)
+                .cornerRadius(10)
+                .padding()
+                
+                Spacer()
+                
+                
+                    
                 
             } // vstack
-            .frame(width: 350, height: 320)
-            .background(.white)
-            .cornerRadius(10)
-            .padding()
             
-            Spacer()
-            
-            
-                
-            
-        }
+        } // scrollview
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .edgesIgnoringSafeArea(.all)
         .background(Color.passFinderBG)
-        
-    }
+
+    } // body
 }
 
 struct MyTypeView_Previews: PreviewProvider {
