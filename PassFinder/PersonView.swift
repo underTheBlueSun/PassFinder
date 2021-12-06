cfd5e0fd07bdc11c35fd9d78f8f505ec1e1d1ccd
underTheBlueSun <wind7129@naver.com>
2021-12-06 21:20:09 +0900

    ..

--- PassFinder/PersonView.swift
+++ PassFinder/PersonView.swift
@@ -14,58 +14,83 @@ struct PersonView: View {
             
             ScrollView {
                 
-                VStack(spacing:0) {
-                    HStack(spacing: 20) {
-                        
-                        Text("엄민욱")
-                            .foregroundColor(.white)
-                            .font(.system(size: 50, weight: .heavy))
-                            .padding(.vertical, 1)
-                        
-//                        Image("ESFJ")
-//                            .resizable()
-//                            .cornerRadius(10)
-//                            .frame(width: 90, height: 90)
-                        
-                        VStack(spacing: 0) {
+                NavigationLink(destination: Text("aaaa")) {
+                    
+                    VStack(spacing:0) {
+                        HStack(spacing: 20) {
                             
-                            Text("ISTP")
-                                .foregroundColor(.systemTeal)
+                            Text("엄민욱")
+                                .foregroundColor(.white)
                                 .font(.system(size: 40, weight: .heavy))
-                                .padding(.vertical, 1)
-                            
-                            Divider().frame(width:110, height: 1).background(Color.white)
-                            
-            //                Text(passFinderModel.filteredArray[passFinderModel.filteredArray.startIndex].title)
-                            Text("잔다르크형")
-                                .foregroundColor(.passFinderItem1)
-                                .font(.system(size: 20, weight: .heavy))
-                                .padding(.vertical, 2)
-                            
-                        } // vstack
-                        
-                        
-                    } // hstack
-                    
-                    
-                } // vstack
-                .frame(width: 340, height: 110)
-                .background(Color.passFinderDark).opacity(0.8)
-                .cornerRadius(10)
-                
-                VStack(spacing:0) {
-                    
-                } // vstack
-                .frame(width: 340, height: 110)
-                .background(Color.passFinderDark).opacity(0.8)
-                .cornerRadius(10)
+                                .padding()
 
+                            VStack(spacing: 0) {
+                                
+                                Text("ISTP")
+                                    .foregroundColor(.systemTeal)
+                                    .font(.system(size: 30, weight: .heavy))
+                                    .padding(.vertical, 1)
+                                
+                                Divider().frame(width:110, height: 1).background(Color.white)
+                                
+                                Text("잔다르크형")
+                                    .foregroundColor(.passFinderItem1)
+                                    .font(.system(size: 18, weight: .heavy))
+                                    .padding(.vertical, 2)
+                                
+                            } // vstack
+                            .padding()
+                            
+                        } // hstack
+                        
+                    } // vstack
+                    .frame(width: 340, height: 80)
+                    .background(Color.passFinderDark).opacity(0.8)
+                    .cornerRadius(10)
+                    
+                } // NavigationLink
                 
+                NavigationLink(destination: Text("aaaa")) {
+                    
+                    VStack(spacing:0) {
+                        HStack(spacing: 20) {
+                            
+                            Text("엄민욱")
+                                .foregroundColor(.white)
+                                .font(.system(size: 40, weight: .heavy))
+                                .padding()
+
+                            VStack(spacing: 0) {
+                                
+                                Text("ISTP")
+                                    .foregroundColor(.systemTeal)
+                                    .font(.system(size: 30, weight: .heavy))
+                                    .padding(.vertical, 1)
+                                
+                                Divider().frame(width:110, height: 1).background(Color.white)
+                                
+                                Text("잔다르크형")
+                                    .foregroundColor(.passFinderItem1)
+                                    .font(.system(size: 18, weight: .heavy))
+                                    .padding(.vertical, 2)
+                                
+                            } // vstack
+                            .padding()
+                            
+                        } // hstack
+                        
+                    } // vstack
+                    .frame(width: 340, height: 80)
+                    .background(Color.passFinderDark).opacity(0.8)
+                    .cornerRadius(10)
+                    
+                } // NavigationLink
+
             } // scrollview
             .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color.passFinderBG)
-            .navigationBarTitle("엄민욱", displayMode: .inline)
+            .navigationBarTitle("다른 사람 유형", displayMode: .inline)
             .navigationBarColor(backgroundColor: UIColor(Color.passFinderBG), tintColor: .white)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
