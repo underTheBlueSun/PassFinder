ede5a4dfb56f95a8fd478d10b884a88c40192a3e
underTheBlueSun <wind7129@naver.com>
2021-12-27 07:38:03 +0900

    ..

--- PassFinder/ContentView.swift
+++ PassFinder/ContentView.swift
@@ -11,47 +11,25 @@ struct ContentView: View {
     
     var body: some View {
         
-        ScrollView {
-            
-            
-            HStack(spacing: 20) {
+        VStack(alignment:.leading) {
+            HStack {
                 
-                Text("민욱")
-                    .frame(width: 120, alignment: .leading)
-                    .foregroundColor(.white)
-                    .font(.system(size: 30, weight: .heavy))
-//                                        .padding()
-                
-                Text("isfp")
-                    .frame(alignment: .trailing)
-                    .foregroundColor(.systemTeal)
-                    .font(.system(size: 35, weight: .heavy))
+                ZStack {
+                    Circle()
+                        .stroke(Color.gray,lineWidth: 2)
+                        .frame(width: 17, height: 17)
+                    
+                   
+                } // zstack
+                    
+                Text("먼저 전화해서 안부를 묻거나 약속을 잡지않는다")
+                    .frame(width: 280, alignment: .leading)
+    //                .foregroundColor(self.isSelected ? Color.passFinderItem1 : Color.white)
+                    .font(.system(size: 17, weight: .heavy))
+    //                        .padding(.vertical, 3)
 
             } // hstack
-            .padding()
-            .frame(width: 250)
-            .background(Color.passFinderDark).opacity(0.8)
-            .cornerRadius(10)
-            
-            HStack(spacing: 20) {
-                
-                Text("민욱가가")
-                    .frame(width: 120, alignment: .leading)
-                    .foregroundColor(.white)
-                    .font(.system(size: 30, weight: .heavy))
-//                                        .padding()
-                
-                Text("isfp")
-                    .frame(alignment: .trailing)
-                    .foregroundColor(.systemTeal)
-                    .font(.system(size: 35, weight: .heavy))
-
-            } // hstack
-            .frame(width: 250)
-            .padding()
-            .background(Color.passFinderDark).opacity(0.8)
-            .cornerRadius(10)
-            
+    //            } // Zstack
         }
         
         
