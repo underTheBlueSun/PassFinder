e1751dd705294c1404406bebd4b86387263b2ebf
underTheBlueSun <wind7129@naver.com>
2021-12-27 16:31:00 +0900

    ..

--- PassFinder/ContentView.swift
+++ PassFinder/ContentView.swift
@@ -11,26 +11,30 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack(alignment:.leading) {
-            HStack {
-                
-                ZStack {
-                    Circle()
-                        .stroke(Color.gray,lineWidth: 2)
-                        .frame(width: 17, height: 17)
+        ScrollView {
+            VStack(alignment:.leading) {
+                HStack {
                     
-                   
-                } // zstack
-                    
-                Text("먼저 전화해서 안부를 묻거나 약속을 잡지않는다")
-                    .frame(width: 280, alignment: .leading)
-    //                .foregroundColor(self.isSelected ? Color.passFinderItem1 : Color.white)
-                    .font(.system(size: 17, weight: .heavy))
-    //                        .padding(.vertical, 3)
+                    ZStack {
+                        Circle()
+                            .stroke(Color.gray,lineWidth: 2)
+                            .frame(width: 17, height: 17)
+                        
+                       
+                    } // zstack
+                        
+                    Text("먼저 전화해서 안부를 묻거나 약속을 잡지않는다")
+                        .frame(width: 280, alignment: .leading)
+        //                .foregroundColor(self.isSelected ? Color.passFinderItem1 : Color.white)
+                        .font(.system(size: 17, weight: .heavy))
+        //                        .padding(.vertical, 3)
 
-            } // hstack
-    //            } // Zstack
+                } // hstack
+        //            } // Zstack
+            }
+            
         }
+        
         
         
         
